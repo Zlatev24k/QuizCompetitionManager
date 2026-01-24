39a7f66f5956a158b8d150f0c2e5076d0f677179
Александър Златев <geoguessr30339@gmail.com>
2026-01-25 00:43:29 +0200

    Added list with all competitions for guests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizCompetitionManager.Data;
 using QuizCompetitionManager.Models;
+using System.Diagnostics;
 
 namespace QuizCompetitionManager.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -21,6 +25,15 @@ namespace QuizCompetitionManager.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        public async Task<IActionResult> Competitions()
+        {
+            var comps = await _db.Competitions
+                .OrderBy(c => c.Status)
+                .OrderByDescending(c => c.StartDateTime)
+                .ToListAsync();
+
+            return View(comps);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
