4f2ff275972b11cfe0d4b2781585a0a155d86e06
Александър Златев <geoguessr30339@gmail.com>
2026-04-28 11:32:10 +0300

    removed most of the requirements for the passwords

--- Program.cs
+++ Program.cs
@@ -22,6 +22,14 @@ namespace QuizCompetitionManager
                 .AddDefaultIdentity<IdentityUser>(options =>
                 {
                     options.SignIn.RequireConfirmedAccount = false;
+
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequiredUniqueChars = 0;
                 })
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
